d92dd66b3e8250302d8f4c1b7e843c9b47959e0d
Steven Lei <474933858@qq.com>
2019-03-04 21:31:39 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -83,10 +83,8 @@ def train(model,
     num_val = int(len(lines)*val_split)
     num_train = len(lines) - num_val
     print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-    if not os.path.isdir(logDirPath):
-        os.makedirs(logDirPath)
-    logging = TensorBoard(log_dir=logDirPath)
-    checkpoint = ModelCheckpoint(logDirPath + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
+    logging = TensorBoard(log_dir=log_dir)
+    checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
         monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
@@ -99,8 +97,9 @@ def train(model,
         initial_epoch=0,
         callbacks=[logging, checkpoint, reduce_lr, early_stopping])
     # when model training finished, save model
-    
-    model_savedPath = 'saved_model/trained_weights.h5'
+    if not os.path.isdir(logDirPath):
+        os.makedirs(logDirPath)
+    model_savedPath = os.path.join(logDirPath, 'trained_weights.h5')
     model.save_weights(model_savedPath)
 
 
